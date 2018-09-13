1208937e9e55e6a40d3758f7737a5d7b757397ae
Shantanu Gade <40289161+shantanug-cuelogic@users.noreply.github.com>
2018-09-14 01:49:46 +0530

    removed modularized css

--- src/App.js
+++ src/App.js
@@ -2,33 +2,32 @@ import React, { Component } from 'react';
 import Editor from './Components/Editor/Editor';
 import ReactDOM from 'react-dom';
 
-// import ReactDOM from 'react-dom';
-// import Button from '@material-ui/core/Button';
-// import SummaryGrid from './Components/Grids/Summary Grid/Summary Grid';
-// import Blog from './Components/Blog/Blog';
-// import Layout from './Components/Layout/Layout';
-// import BlogBuilder from './Containers/BlogBuilder/BlogBuilder'; 
-// import { MuiThemeProvider,createMuiTheme } from '@material-ui/core/styles';
-// import purple from '@material-ui/core/colors/purple';
-// import { StickyContainer, Sticky } from 'react-sticky';
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import SummaryGrid from './Components/Grids/Summary Grid/Summary Grid';
+import Blog from './Components/Blog/Blog';
+import Layout from './Components/Layout/Layout';
+import BlogBuilder from './Containers/BlogBuilder/BlogBuilder'; 
+import { MuiThemeProvider,createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import { StickyContainer, Sticky } from 'react-sticky';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       main:'#004D40',
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:'#004D40',
       
-//     },
-//     secondary: {
-//       main: '#f44336',
-//     },
-//     mycolor:{
-//       main:'#263238'
-//     },
-//   },
-// });
+    },
+    secondary: {
+      main: '#f44336',
+    },
+    mycolor:{
+      main:'#263238'
+    },
+  },
+});
 
 
 
@@ -40,14 +39,15 @@ class App extends Component {
 
     return (
       <div className="App">
-{/*       
-      <MuiThemeProvider theme={theme}>
-         <Layout> */}
-          {/* <BlogBuilder /> */}
+       
+       <MuiThemeProvider theme={theme}>
+          <div style={{textAlign:'center',width:'70%', marginLeft:'15%' , marginRight:'15%' }}>
+          <Layout /> 
+          <BlogBuilder />
           <Editor/>
-{/*           
-          </Layout>
-          </MuiThemeProvider> */}
+          </div>
+          </MuiThemeProvider>
+       
      </div>  
     );
   }
