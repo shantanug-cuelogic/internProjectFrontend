4db6e09fd7a759283f846fa85f13cc64cf1a339d
shantanu <shantanu.gade@cuelogic>
2018-10-10 18:15:09 +0530

    added higherordercomponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import { withRouter } from 'react-router'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import * as actionTypes from './Store/Actions/actionTypes';
+
 import SignIn from './Components/Sign In/Sign In';
 import SignUp from './Components/Sign Up/Sign Up';
 import BlogBuilder from './Containers/BlogBuilder/BlogBuilder';
@@ -10,9 +14,6 @@ import Editor from './Containers/Editor/Editor';
 import Layout from './Components/Layout/Layout';
 import Drafts from './Components/Drafts/Drafts';
 import DraftEditor from './Containers/Editor/DraftEditor';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
-import * as actionTypes from './Store/Actions/actionTypes';
 import EditPost from './Containers/Post/EditPost';
 import Dashboard from './Containers/Dashboard/Dashboard';
 import Category from './Containers/Category/Category';
@@ -21,8 +22,12 @@ import AuthorProfile from './Containers/AuthorProfile/AuthorProfile';
 import ForgotPassword from './Components/ForgotPassword/ForgotPassword';
 import PasswordRecover from './Components/PasswordRecover/PasswordRecover';
 import UpdateProfile from './Components/UpdateProfile/UpdateProfile';
+import HigherOrderComponent from './HigherOrderComponent/HigherOrderComponent';
 import Snackbar from '@material-ui/core/Snackbar';
 import UserService from './Services/UserService';
+
+import DarkTheme from './Themes/DarkTheme';
+import DefaultTheme from './Themes/DefaultTheme';
 
 class App extends Component {
   componentDidMount = async () => {
@@ -48,75 +53,44 @@ class App extends Component {
     this.props.closeSnackBar();
 
   };
-
   render() {
+    let authenticatedRoutes = null;
+    if (this.props.auth) {
+      authenticatedRoutes =
+        <HigherOrderComponent>
+          <Route path='/editor' component={Editor}></Route>
+          <Route path='/profile' component={Profile}></Route>
+          <Route path='/createpost' component={Editor} ></Route>
+          <Route path='/dashboard' component={Dashboard} ></Route>
+          <Route path="/updateprofile" component={UpdateProfile}> </Route>
+          <Route path="/drafts" component={Drafts}> </Route>
+          <Route path="/drafteditor/:id" component={DraftEditor}> </Route>
+        </HigherOrderComponent>
+
+
+    }
     let updateRoute = null;
+
     if (this.props.postUserId === this.props.userId) {
       updateRoute = <Route path='/editpost/:id' component={EditPost}></Route>
     }
-
-    const theme =
-      this.props.isDark ?
-        createMuiTheme({
-          palette: {
-            primary: {
-              light: '#616161',
-              main: '#212121',
-              dark: '#212121',
-              contrastText: '#fff',
-            },
-            secondary: {
-              light: '#ff7961',
-              main: '#f44336',
-              dark: '#ba000d',
-              contrastText: '#000',
-            },
-          },
-        }) :
-        createMuiTheme({
-          palette: {
-            primary: {
-              light: '#757ce8',
-              main: '#3f50b5',
-              dark: '#002884',
-              contrastText: '#fff',
-            },
-            secondary: {
-              light: '#ff7961',
-              main: '#f44336',
-              dark: '#ba000d',
-              contrastText: '#000',
-            },
-          },
-        })
-
-
-
+    const theme = this.props.isDark ? createMuiTheme(DarkTheme) : createMuiTheme(DefaultTheme);
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
-
-
           <Layout logout={this.logout} />
           <div style={{ textAlign: 'center', width: '90%', marginLeft: '5%', marginRight: '5%' }}>
             <Switch>
               <Route path='/signin' component={SignIn}></Route>
-              <Route path='/editor' component={Editor}></Route>
               <Route path='/signup' component={SignUp}></Route>
-              <Route path='/profile' component={Profile}></Route>
               <Route path='/post/:id' component={Post} ></Route>
               {updateRoute}
-
-              <Route path='/createpost' component={Editor} ></Route>
-              <Route path='/dashboard' component={Dashboard} ></Route>
+              {authenticatedRoutes}
               <Route path='/category/:id' component={Category} ></Route>
               <Route path='/search' component={SearchPost} ></Route>
               <Route path='/authorprofile/:userId' component={AuthorProfile} ></Route>
               <Route path='/forgotpassword' component={ForgotPassword}></Route>
               <Route path='/recoverpassword/:authToken' component={PasswordRecover}></Route>
-              <Route path="/updateprofile" component={UpdateProfile}> </Route>
-              <Route path="/drafts" component={Drafts}> </Route>
-              <Route path="/drafteditor/:id" component={DraftEditor}> </Route>
               <Route path='/' exact component={BlogBuilder}></Route>
             </Switch>
           </div>
---
+++ src/Themes/DarkTheme.js
@@ -0,0 +1,18 @@
+
+   const dark = {
+    palette: {
+      primary: {
+        light: '#616161',
+        main: '#212121',
+        dark: '#212121',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  }
+  export default dark;
