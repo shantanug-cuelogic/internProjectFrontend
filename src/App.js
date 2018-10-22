80e6eafafba33e647c679cbb3ea013f55b57aa1d
shantanu <shantanu.gade@cuelogic>
2018-10-22 19:23:34 +0530

    added dynamic imports

--- src/App.js
+++ src/App.js
@@ -4,30 +4,14 @@ import { withRouter } from 'react-router'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import * as actionTypes from './Store/Actions/actionTypes';
-
-import SignIn from './Components/Sign In/Sign In';
-import SignUp from './Components/Sign Up/Sign Up';
-import BlogBuilder from './Containers/BlogBuilder/BlogBuilder';
-import Post from './Containers/Post/Post';
-import Profile from './Components/Profile/Profile';
-import Editor from './Containers/Editor/Editor';
+import asyncImports from './Utility/asyncImports';
 import Layout from './Components/Layout/Layout';
-import Drafts from './Components/Drafts/Drafts';
-import DraftEditor from './Containers/Editor/DraftEditor';
-import EditPost from './Containers/Post/EditPost';
-import Dashboard from './Containers/Dashboard/Dashboard';
-import Category from './Containers/Category/Category';
-import SearchPost from './Components/SearchPost/SearchPost';
-import AuthorProfile from './Containers/AuthorProfile/AuthorProfile';
-import ForgotPassword from './Components/ForgotPassword/ForgotPassword';
-import PasswordRecover from './Components/PasswordRecover/PasswordRecover';
-import UpdateProfile from './Components/UpdateProfile/UpdateProfile';
-import HigherOrderComponent from './HigherOrderComponent/HigherOrderComponent';
 import Snackbar from '@material-ui/core/Snackbar';
 import UserService from './Services/UserService';
-
 import DarkTheme from './Themes/DarkTheme';
 import DefaultTheme from './Themes/DefaultTheme';
+
+
 
 class App extends Component {
   componentDidMount = async () => {
@@ -54,55 +38,59 @@ class App extends Component {
 
   };
   render() {
-    // let authenticatedRoutes = null;
-    // if (this.props.auth) {
-    //   authenticatedRoutes =
-    //     <Switch>
-    //       <Route path='/editor' component={Editor}></Route>
-    //       <Route path='/profile' component={Profile}></Route>
-    //       <Route path='/createpost' component={Editor} ></Route>
-    //       <Route path='/dashboard' component={Dashboard} ></Route>
-    //       <Route path="/updateprofile" component={UpdateProfile}> </Route>
-    //       <Route path="/drafts" component={Drafts}> </Route>
-    //       <Route path="/drafteditor/:id" component={DraftEditor}> </Route>
-    //     </Switch>
+    let authenticatedRoutes = null;
+    if (this.props.auth) {
+      authenticatedRoutes =
+        <Switch>
+          <Route path='/signin' component={asyncImports.asyncSignIn}></Route>
+          <Route path='/signup' component={asyncImports.asyncSignUp}></Route>
+          <Route path='/post/:id' component={asyncImports.asyncPost} ></Route>
+          <Route path='/editor' component={asyncImports.asyncEditor}></Route>
+          <Route path='/profile' component={asyncImports.asyncProfile}></Route>
+          <Route path='/createpost' component={asyncImports.asyncEditor} ></Route>
+          <Route path='/dashboard' component={asyncImports.asyncDashboard} ></Route>
+          <Route path="/updateprofile" component={asyncImports.asyncUpdateProfile}> </Route>
+          <Route path="/drafts" component={asyncImports.asyncDrafts}> </Route>
+          <Route path="/drafteditor/:id" component={asyncImports.asyncDraftEditor}> </Route>
+          <Route path='/category/:id' component={asyncImports.asyncCategory} ></Route>
+          <Route path='/search' component={asyncImports.asyncSearchPost} ></Route>
+          <Route path='/authorprofile/:userId' component={asyncImports.asyncAuthorProfile} ></Route>
+          <Route path='/forgotpassword' component={asyncImports.asyncForgotPassword}></Route>
+          <Route path='/recoverpassword/:authToken' component={asyncImports.asyncPasswordRecover}></Route>
+          <Route path='/' exact component={asyncImports.asyncBlogBuilder}></Route>
+        </Switch>
+    }
+    else {
+      authenticatedRoutes =
+        <Switch>
+          <Route path='/signin' component={asyncImports.asyncSignIn}></Route>
+          <Route path='/signup' component={asyncImports.asyncSignUp}></Route>
+          <Route path='/post/:id' component={asyncImports.asyncPost} ></Route>
+          <Route path='/category/:id' component={asyncImports.asyncCategory} ></Route>
+          <Route path='/search' component={asyncImports.asyncSearchPost} ></Route>
+          <Route path='/authorprofile/:userId' component={asyncImports.asyncAuthorProfile} ></Route>
+          <Route path='/forgotpassword' component={asyncImports.asyncForgotPassword}></Route>
+          <Route path='/recoverpassword/:authToken' component={asyncImports.asyncPasswordRecover}></Route>
+          <Route path='/' exact component={asyncImports.asyncBlogBuilder}></Route>
+        </Switch>
+    }
 
+    let updateRoute = null;
 
-    // }
-    // let updateRoute = null;
-
-    // if (this.props.postUserId === this.props.userId) {
-    //   updateRoute = <Route path='/editpost/:id' component={EditPost}></Route>
-    // }
+    if (this.props.postUserId === this.props.userId) {
+      updateRoute = <Route path='/editpost/:id' component={asyncImports.asyncEditPost}></Route>
+    }
     const theme = this.props.isDark ? createMuiTheme(DarkTheme) : createMuiTheme(DefaultTheme);
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
           <Layout logout={this.logout} />
           <div style={{ textAlign: 'center', width: '90%', marginLeft: '5%', marginRight: '5%' }}>
-        {/* <div id="google_translate_element" style={{ display: 'inline', height: 27, float: 'right' , marginTop:'10%' }} ></div> */}
+            {/* <div id="google_translate_element" style={{ display: 'inline', height: 27, float: 'right' , marginTop:'10%' }} ></div> */}
 
-            <Switch>
-              <Route path='/signin' component={SignIn}></Route>
-              <Route path='/signup' component={SignUp}></Route>
-              <Route path='/post/:id' component={Post} ></Route>
-              <Route path='/editpost/:id' component={EditPost}></Route>
-              <Route path='/editor' component={Editor}></Route>
-              <Route path='/profile' component={Profile}></Route>
-              <Route path='/createpost' component={Editor} ></Route>
-              <Route path='/dashboard' component={Dashboard} ></Route>
-              <Route path="/updateprofile" component={UpdateProfile}> </Route>
-              <Route path="/drafts" component={Drafts}> </Route>
-              <Route path="/drafteditor/:id" component={DraftEditor}> </Route>
-              {/* {updateRoute}
-             {authenticatedRoutes} */}
-              <Route path='/category/:id' component={Category} ></Route>
-              <Route path='/search' component={SearchPost} ></Route>
-              <Route path='/authorprofile/:userId' component={AuthorProfile} ></Route>
-              <Route path='/forgotpassword' component={ForgotPassword}></Route>
-              <Route path='/recoverpassword/:authToken' component={PasswordRecover}></Route>
-              <Route path='/' exact component={BlogBuilder}></Route>
-            </Switch>
+              {updateRoute}
+              {authenticatedRoutes}
+             
           </div>
           <Snackbar
             anchorOrigin={{
